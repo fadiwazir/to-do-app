93de90f32f9f0becbbdc251eb6f22c24a68a568e
Fadi Alshareef <166608344+fadiwazir@users.noreply.github.com>
2026-01-12 03:49:16 +0200

    use local storage

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ let submitBtn = document.getElementById("sub");
 let tasksDiv = document.getElementById("tasks");
 
 let arrayTasks = [];
+
 function fillTask() {
   tasksDiv.innerHTML = "";
   let index = 0;
@@ -21,28 +22,47 @@ function fillTask() {
   }
   input.value = "";
 }
+// get item from local storage
+arrayTasks = JSON.parse(localStorage.getItem("task"));
+// end
 fillTask();
 let index = 0;
 submitBtn.addEventListener("click", () => {
-  let taskObj = {
-    title: input.value,
-  };
-  arrayTasks.push(taskObj);
-  fillTask();
+  if (input.value !== "") {
+    let taskObj = {
+      title: input.value,
+    };
+    arrayTasks.push(taskObj);
+
+    // local storage 
+    storeTask()
+
+    fillTask();
+  }
 });
 
 function delTask(index) {
-  let tasks = arrayTasks[index]
+  let tasks = arrayTasks[index];
   let x = confirm(`You want delete the: ${tasks.title} !`);
   if (x == true) {
     arrayTasks.splice(index, 1);
+    storeTask()
     fillTask();
   }
 }
 
-function editTask(index){
-  let tasks = arrayTasks[index]
-  let newName = prompt(`${tasks.title}`)
-  tasks.title = newName;
-  fillTask();
+function editTask(index) {
+  let tasks = arrayTasks[index];
+  let newName = prompt(`${tasks.title}`);
+  if (newName !== "") {
+    tasks.title = newName;
+    storeTask()
+    fillTask();
+  }
+}
+
+// storage function
+function storeTask() {
+  let tasktoString = JSON.stringify(arrayTasks);
+  localStorage.setItem("task", tasktoString);
 }
